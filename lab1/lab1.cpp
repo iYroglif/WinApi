a264c5a33cbfd99ba688e48ac41b968c2e00be79
iYroglif <webman.com@yandex.ru>
2020-06-02 12:40:47 +0300

    lab1

---
+++ lab1/lab1.cpp
@@ -0,0 +1,115 @@
+﻿#include <Windows.h>
+#include <tchar.h>
+#include <math.h>
+
+LRESULT CALLBACK WndProc(HWND, UINT, WPARAM, LPARAM);
+
+TCHAR WinName[] = _T("MainFrame");
+
+HDC hdc;
+
+int WINAPI _tWinMain(HINSTANCE This,		 // Дескриптор текущего приложения 
+	HINSTANCE Prev, 	// В современных системах всегда 0 
+	LPTSTR cmd, 		// Командная строка 
+	int mode) 		// Режим отображения окна
+{
+	HWND hWnd;		// Дескриптор главного окна программы 
+	MSG msg; 		// Структура для хранения сообщения 
+	WNDCLASS wc; 	// Класс окна
+
+	// Определение класса окна 
+	wc.hInstance = This;
+	wc.lpszClassName = WinName; 				// Имя класса окна 
+	wc.lpfnWndProc = WndProc; 					// Функция окна 
+	wc.style = CS_HREDRAW | CS_VREDRAW; 			// Стиль окна 
+	wc.hIcon = LoadIcon(NULL, IDI_APPLICATION); 		// Стандартная иконка 
+	wc.hCursor = LoadCursor(NULL, IDC_ARROW); 		// Стандартный курсор 
+	wc.lpszMenuName = NULL; 					// Нет меню 
+	wc.cbClsExtra = 0; 						// Нет дополнительных данных класса 
+	wc.cbWndExtra = 0; 						// Нет дополнительных данных окна 
+	//wc.hbrBackground = (HBRUSH)(COLOR_WINDOW + 1); 	// Заполнение окна белым цветом 
+	wc.hbrBackground = CreateSolidBrush(RGB(250, 218, 221));
+
+
+	// Регистрация класса окна
+	if (!RegisterClass(&wc)) return 0;
+
+	// Создание окна 
+	hWnd = CreateWindow(WinName,			// Имя класса окна 
+		_T("Каркас Windows-приложения"), 		// Заголовок окна 
+		WS_OVERLAPPEDWINDOW, 		// Стиль окна 
+		CW_USEDEFAULT,				// x 
+		CW_USEDEFAULT, 				// y	 Размеры окна 
+		CW_USEDEFAULT, 				// width 
+		CW_USEDEFAULT, 				// Height 
+		HWND_DESKTOP, 				// Дескриптор родительского окна 
+		NULL, 						// Нет меню 
+		This, 						// Дескриптор приложения 
+		NULL); 					// Дополнительной информации нет 
+
+	ShowWindow(hWnd, mode); 				// Показать окно
+
+	// Цикл обработки сообщений 
+	while (GetMessage(&msg, NULL, 0, 0))
+	{
+		TranslateMessage(&msg); 		// Функция трансляции кодов нажатой клавиши 
+		DispatchMessage(&msg); 		// Посылает сообщение функции WndProc() 
+	}
+	return 0;
+}
+
+// Оконная функция вызывается операционной системой
+// и получает сообщения из очереди для данного приложения
+
+LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
+{
+
+	static int a=7, b=3, c = 3, x, y;
+
+	switch (message)		 // Обработчик сообщений
+	{
+		
+	case WM_PAINT:
+	{
+		PAINTSTRUCT ps;
+		hdc = BeginPaint(hWnd, &ps);
+		HPEN hPen1 = CreatePen(PS_SOLID, a, RGB(0, 0, 0));
+		SelectObject(hdc, hPen1);
+		HBRUSH hBrush = (HBRUSH)GetStockObject(HOLLOW_BRUSH);
+		SelectObject(hdc, hBrush);
+		Ellipse(hdc, 10, 10, 360, 260);
+
+		HPEN hPen2 = CreatePen(PS_SOLID, b, RGB(0, 0, 255));
+		SelectObject(hdc, hPen2);
+		Ellipse(hdc, 60, 60, 310, 210);
+
+		HPEN hPen3 = CreatePen(PS_SOLID, c, RGB(255, 255, 0));
+		SelectObject(hdc, hPen3);
+		Ellipse(hdc, 110, 110, 260, 160);
+
+
+		EndPaint(hWnd, &ps);
+		break;
+	}
+	
+	case WM_LBUTTONUP:
+	{
+		x = LOWORD(lParam) - 185;
+		y = -HIWORD(lParam) + 135;
+		if ((((x * x) / (75.0 * 75)) + ((y * y) / (25.0 * 25))) <= 1)
+			c++;
+		else if (((x * x) / (125.0 * 125) + (y * y) / (75.0 * 75)) <= 1)
+			b++;
+		else if (((x * x) / (175.0 * 175) + (y * y) / (125.0 * 125)) <= 1)
+			a++;
+		InvalidateRect(hWnd, NULL, true);
+		break;
+	}
+	case WM_DESTROY:
+		PostQuitMessage(0);
+		break; 			// Завершение программы 
+	default: 			// Обработка сообщения по умолчанию 
+		return DefWindowProc(hWnd, message, wParam, lParam);
+	}
+	return 0;
+}
